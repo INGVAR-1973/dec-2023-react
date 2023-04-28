0c53e6cfa1fb8fbdb4c00ec8fa85f3674e59c6bf
igor <harakternic@gmail.com>
2023-04-28 15:20:13 +0300

    hw-2.2

--- src/components/CarForm/CarForm.js
+++ src/components/CarForm/CarForm.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
+import {carService} from "../../services/car.service";
 
-const CarForm = () => {
-    const {register,handleSubmit,reset,formState:{errors,isValid}}=useForm();
-    const save=(car)=>{
-        console.log(car);
+const CarForm = ({setAllCars,carForUpdate}) => {
+    const {register,handleSubmit,reset,formState:{errors,isValid},setValue}=useForm({mode:'all'});
+    useEffect(() => {
+        if (carForUpdate){
+            setValue('brand',carForUpdate.brand,{shouldValidate:true})
+            setValue('price',carForUpdate.price,{shouldValidate:true})
+            setValue('year',carForUpdate.year,{shouldValidate:true})
+        }
+    },deps:[carForUpdate])
+    const save= async (car)=>{
+        const {data}=await carService.create(car);
+        setAllCars(prev=>!prev)
+        reset()
+
+
+    }
+    const update = async (car) =>{
+
     }
     return (
         <form onSubmit={handleSubmit(save)}>
-            <input type="text"placeholder={'brand'} {...register('brand')}/>
-            <input type="text"placeholder={'price'} {...register('price')}/>
-            <input type="text"placeholder={'year'} {...register('year')}/>
+            <input type="text"placeholder={'brand'} {...register('brand',{
+                pattern: {value:/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/,
+                message:'Бренд має складатися тільки з літер мін 1 і макс 20 літер'}})} />
+            {errors.brand && <span>{errors.brand.message}</span>}
+            <input type="text"placeholder={'price'} {...register('price',{
+                valueAsNumber:true,min:{value:0,message:'min 0'},max:{value:1000000}})}/>
+            {errors.price&&<span>{errors.price.message}</span>}
+            <input type="text"placeholder={'year'} {...register('year',{
+                valueAsNumber:true})}/>
+
             <button>Save</button>
+
 
         </form>
     );
--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -1,15 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import {carService} from "../../services/car.service";
 import {Car} from "../Car/Car";
+import {CarForm} from "../CarForm/CarForm";
 
 const Cars = () => {
     const [cars,setCars] = useState([]);
-    useEffect(()=>{
+    const [allCars,setAllCars]=useState(null);
+   const [carForUpdate,setCarForUpdate]=useState(null);
+    useEffect(() => {
         carService.getAll().then(value=>value.data).then(value=>setCars(value))
-    })
+    },deps:[allCars])
     return (
         <div>
-            {cars.map(car=>< Car key ={car.id} car={car}/>)}
+            <CarForm setAllCars={setAllCars} carForUpdate={carForUpdate}/>
+            <hr/>
+            {cars.map(car=>< Car key ={car.id} car={car} setCarForUpdate={setCarForUpdate()}/>)}
         </div>
     );
 };
